e1da3649e3c8046cd30c7ecf073a7b98eb006b1a
rpounceyjr <rpounceyjr@gmail.com>
2020-05-18 16:15:33 -0400

    in main.handlebars changed the jquery cdn to the full version, not the slim which does not have post functionality.  added a create character button to the create-character.handlebars so I could submit character info.  Created client-side route for character createion post method in main.js.  Does not yet pass all info, but does pass UserId and name, just need to flesh it out with the other inputs.  post method for creating a character in api-routes.js works, pushing info to database.

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,19 @@
-//Gets the user ID from the query string
+// Gets the user ID from the query string
 let userID = window.location.search.split("=")[1];
 
+// Route to post a new character
+$("#new-character-submit").on("click", createNewCharacter);
+
+// Function to create and post a new character
+function createNewCharacter(event) {
+    event.preventDefault();
+    const newCharacter = {
+        characterName: $("#charname").val().trim(),
+        UserId: userID
+    };
+
+    $.post("/api/characters", newCharacter)
+        .then(function (data) {
+            console.log(data);
+        });
+};
