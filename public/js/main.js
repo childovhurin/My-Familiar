4fa2fc34f8ff606259048ec059a045b997b0b6ec
rpounceyjr <rpounceyjr@gmail.com>
2020-05-22 14:53:16 -0400

    finished update and delete routes.  All routes are working.  Still unable to get values from text-areas, need to change to inputs.

--- public/js/main.js
+++ public/js/main.js
@@ -9,8 +9,8 @@ $(document).ready(function () {
     if (firstQueryLetter === "c") {
         charID = query;
         console.log("charID:" + charID)
-        $.get("/api/" + charID)
-        .then((data) => console.log(data));
+        // $.get("/api/" + charID)
+        //     .then((data) => console.log(data));
     } else {
         userID = query;
         console.log("userID:" + userID);
@@ -98,7 +98,101 @@ function createNewCharacter(event) {
         });
 };
 
-// Delete button listener
-// deleteCharacterButton.on("click", () )
+// Ajax function to delete a character
+const deleteCharacter = function (id) {
+    console.log("this is the delete character id: " + id);
+    return $.ajax({
+        url: "/api/characters/" + id,
+        method: "DELETE"
+    })
+        .then(() => {
+            alert("Character has been deleted.");
+            window.location.href = "/members";
+        });
+};
 
+// Delete button listener
+deleteCharacterButton.on("click", () => {
+    deleteCharacter(charID);
+});
+
+function updateCharacter(event) {
+    event.preventDefault();
+
+    let updatedCharacter = {
+        characterName: $("#charname").val(),
+        class: $("#class").val(),
+        level: $("#level").val(),
+        race: $("#race").val(),
+        alignment: $("#alignment").val(),
+        experiencePoints: $("#experiencepoints").val(),
+        strength: $("#basestrength").val(),
+        dexterity: $("#dexterity").val(),
+        constitution: $("#consitution").val(),
+        intelligence: $("#intelligence").val(),
+        wisdom: $("#wisdom").val(),
+        charisma: $("#charisma").val(),
+        currentHitPoints: $("#current_hitpoints").val(),
+        armorClass: $("#armor_class").val(),
+        initiative: $("#initiative").val(),
+        speed: $("#base_speed").val(),
+        hitDie: $("#hit_die").val(),
+        languages: $("#languages").val(),
+        acrobatics: $("#acrobatics").val(),
+        animalHandling: $("#animalhandling").val(),
+        arcana: $("#arcana").val(),
+        athletics: $("#athletics").val(),
+        deception: $("#deception").val(),
+        history: $("#history").val(),
+        insight: $("#insight").val(),
+        intimidation: $("#intimidation").val(),
+        investigation: $("#investigation").val(),
+        medicine: $("#medicine").val(),
+        nature: $("#nature").val(),
+        perception: $("#perception").val(),
+        performance: $("#performance").val(),
+        persuasion: $("#persuasion").val(),
+        religion: $("#religion").val(),
+        sleightOfHand: $("#sleight_of_hand").val(),
+        stealth: $("#stealth").val(),
+        survival: $("#survival").val(),
+        personalityTraits: $("#personality_traits").val(),
+        ideals: $("#ideals").val(),
+        bonds: $("#bonds").val(),
+        flaws: $("#flaws").val(),
+        featuresAndTraits: $("#features_and_traits").val(),
+        weapon: $("#weapon").val(),
+        attackBonus: $("#attack_bonus").val(),
+        damage: $("#damage").val(),
+        damageType: $("#damage_type").val(),
+        spellName: $("#spell_name").val(),
+        spellID: $("#spell_id").val(),
+        spellType: $("#spell_type").val(),
+        spellLevel: $("#spell_level").val(),
+        castingTime: $("#casting_time").val(),
+        spellRange: $("#spell_range").val(),
+        spellComponents: $("#spell_components").val(),
+        spellDuration: $("#spell_duration").val(),
+        UserId: userID
+    };
+
+    // Post new character then redirect to members page
+    console.log(updatedCharacter);
+    function updateRequest(id, character) {
+        return $.ajax({
+            url: "/api/characters/update/" + id,
+            method: "PUT",
+            data: character
+        })
+            .then(() => {
+                alert("Character has been updated.");
+                window.location.href = "/members";
+            });
+    };
+
+    updateRequest(charID, updatedCharacter);
+};
 // Update button listener
+updateCharacterButton.on("click", () => {
+    updateCharacter(event);
+});
