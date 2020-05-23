9b8cb80a8cb0c4867adfe3148efde62b9b087b03
aaivey22 <angelawings22@gmail.com>
2020-05-23 11:56:23 -0400

    more dice updates

--- public/js/main.js
+++ public/js/main.js
@@ -3,18 +3,18 @@ let userID;
 let charID;
 
 $(document).ready(function () {
-    const query = window.location.search.split("=")[1]
-    const firstQueryLetter = window.location.search[1];
-    console.log(firstQueryLetter);
-    if (firstQueryLetter === "c") {
-        charID = query;
-        console.log("charID:" + charID)
-        $.get("/api/" + charID)
-        .then((data) => console.log(data));
-    } else {
-        userID = query;
-        console.log("userID:" + userID);
-    }
+  const query = window.location.search.split("=")[1]
+  const firstQueryLetter = window.location.search[1];
+  console.log(firstQueryLetter);
+  if (firstQueryLetter === "c") {
+    charID = query;
+    console.log("charID:" + charID)
+    $.get("/api/" + charID)
+      .then((data) => console.log(data));
+  } else {
+    userID = query;
+    console.log("userID:" + userID);
+  }
 });
 // const userID = window.location.search.split("=")[1];
 const deleteCharacterButton = $("#delete_character_button");
@@ -27,112 +27,113 @@ $("#new-character-submit").on("click", createNewCharacter);
 $("#create-new-character").on("click", () => window.location.href = `/create-character?user_id=${userID}`);
 $("#view-character").on("click", () => window.location.href = `/view-character?user_id=${userID}`);
 
+
 //Route to generate a random number from the d20 dice click 
-$("#d20").on("click", () => { 
-    console.log("click");
-    const roll = Math.floor((Math.random() * 20) + 1);
-    $("#rolled-result").html(`${roll}`)
-  });
-  $("#d12").on("click", () => { 
-    console.log("click");
-    const roll = Math.floor((Math.random() * 12) + 1);
-    $("#rolled-result").html(`${roll}`)
-  });
-  $("#d10").on("click", () => { 
-    console.log("click");
-    const roll = Math.floor((Math.random() * 10) + 1);
-    $("#rolled-result").html(`${roll}`)
-  });
-  $("#d8").on("click", () => { 
-    console.log("click");
-    const roll = Math.floor((Math.random() * 8) + 1);
-    $("#rolled-result").html(`${roll}`)
-  });
-  $("#d6").on("click", () => { 
-    console.log("click");
-    const roll = Math.floor((Math.random() * 6) + 1);
-    $("#rolled-result").html(`${roll}`)
-  });
-  $("#d4").on("click", () => { 
-    console.log("click");
-    const roll = Math.floor((Math.random() * 4) + 1);
-    $("#rolled-result").html(`${roll}`)
-  });
+$("#d20").on("click", () => {
+  console.log("click");
+  const roll = Math.floor((Math.random() * 20) + 1);
+  $("#rolled-result").html(`${roll}`)
+});
+$("#d12").on("click", () => {
+  console.log("click");
+  const roll = Math.floor((Math.random() * 12) + 1);
+  $("#rolled-result").html(`${roll}`)
+});
+$("#d10").on("click", () => {
+  console.log("click");
+  const roll = Math.floor((Math.random() * 10) + 1);
+  $("#rolled-result").html(`${roll}`)
+});
+$("#d8").on("click", () => {
+  console.log("click");
+  const roll = Math.floor((Math.random() * 8) + 1);
+  $("#rolled-result").html(`${roll}`)
+});
+$("#d6").on("click", () => {
+  console.log("click");
+  const roll = Math.floor((Math.random() * 6) + 1);
+  $("#rolled-result").html(`${roll}`)
+});
+$("#d4").on("click", () => {
+  console.log("click");
+  const roll = Math.floor((Math.random() * 4) + 1);
+  $("#rolled-result").html(`${roll}`)
+});
 
 // // Function to create and post a new character
 function createNewCharacter(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    let newCharacter = {
-        characterName: $("#charname").val(),
-        class: $("#class").val(),
-        level: $("#level").val(),
-        race: $("#race").val(),
-        alignment: $("#alignment").val(),
-        experiencePoints: $("#experiencepoints").val(),
-        strength: $("#basestrength").val(),
-        dexterity: $("#dexterity").val(),
-        constitution: $("#consitution").val(),
-        intelligence: $("#intelligence").val(),
-        wisdom: $("#wisdom").val(),
-        charisma: $("#charisma").val(),
-        currentHitPoints: $("#current_hitpoints").val(),
-        armorClass: $("#armor_class").val(),
-        initiative: $("#initiative").val(),
-        speed: $("#base_speed").val(),
-        hitDie: $("#hit_die").val(),
-        languages: $("#languages").val(),
-        acrobatics: $("#acrobatics").val(),
-        animalHandling: $("#animalhandling").val(),
-        arcana: $("#arcana").val(),
-        athletics: $("#athletics").val(),
-        deception: $("#deception").val(),
-        history: $("#history").val(),
-        insight: $("#insight").val(),
-        intimidation: $("#intimidation").val(),
-        investigation: $("#investigation").val(),
-        medicine: $("#medicine").val(),
-        nature: $("#nature").val(),
-        perception: $("#perception").val(),
-        performance: $("#performance").val(),
-        persuasion: $("#persuasion").val(),
-        religion: $("#religion").val(),
-        sleightOfHand: $("#sleight_of_hand").val(),
-        stealth: $("#stealth").val(),
-        survival: $("#survival").val(),
-        personalityTraits: $("#personality_traits").val(),
-        ideals: $("#ideals").val(),
-        bonds: $("#bonds").val(),
-        flaws: $("#flaws").val(),
-        featuresAndTraits: $("#features_and_traits").val(),
-        weapon: $("#weapon").val(),
-        attackBonus: $("#attack_bonus").val(),
-        damage: $("#damage").val(),
-        damageType: $("#damage_type").val(),
-        spellName: $("#spell_name").val(),
-        spellID: $("#spell_id").val(),
-        spellType: $("#spell_type").val(),
-        spellLevel: $("#spell_level").val(),
-        castingTime: $("#casting_time").val(),
-        spellRange: $("#spell_range").val(),
-        spellComponents: $("#spell_components").val(),
-        spellDuration: $("#spell_duration").val(),
-        UserId: userID
-    };
+  let newCharacter = {
+    characterName: $("#charname").val(),
+    class: $("#class").val(),
+    level: $("#level").val(),
+    race: $("#race").val(),
+    alignment: $("#alignment").val(),
+    experiencePoints: $("#experiencepoints").val(),
+    strength: $("#basestrength").val(),
+    dexterity: $("#dexterity").val(),
+    constitution: $("#consitution").val(),
+    intelligence: $("#intelligence").val(),
+    wisdom: $("#wisdom").val(),
+    charisma: $("#charisma").val(),
+    currentHitPoints: $("#current_hitpoints").val(),
+    armorClass: $("#armor_class").val(),
+    initiative: $("#initiative").val(),
+    speed: $("#base_speed").val(),
+    hitDie: $("#hit_die").val(),
+    languages: $("#languages").val(),
+    acrobatics: $("#acrobatics").val(),
+    animalHandling: $("#animalhandling").val(),
+    arcana: $("#arcana").val(),
+    athletics: $("#athletics").val(),
+    deception: $("#deception").val(),
+    history: $("#history").val(),
+    insight: $("#insight").val(),
+    intimidation: $("#intimidation").val(),
+    investigation: $("#investigation").val(),
+    medicine: $("#medicine").val(),
+    nature: $("#nature").val(),
+    perception: $("#perception").val(),
+    performance: $("#performance").val(),
+    persuasion: $("#persuasion").val(),
+    religion: $("#religion").val(),
+    sleightOfHand: $("#sleight_of_hand").val(),
+    stealth: $("#stealth").val(),
+    survival: $("#survival").val(),
+    personalityTraits: $("#personality_traits").val(),
+    ideals: $("#ideals").val(),
+    bonds: $("#bonds").val(),
+    flaws: $("#flaws").val(),
+    featuresAndTraits: $("#features_and_traits").val(),
+    weapon: $("#weapon").val(),
+    attackBonus: $("#attack_bonus").val(),
+    damage: $("#damage").val(),
+    damageType: $("#damage_type").val(),
+    spellName: $("#spell_name").val(),
+    spellID: $("#spell_id").val(),
+    spellType: $("#spell_type").val(),
+    spellLevel: $("#spell_level").val(),
+    castingTime: $("#casting_time").val(),
+    spellRange: $("#spell_range").val(),
+    spellComponents: $("#spell_components").val(),
+    spellDuration: $("#spell_duration").val(),
+    UserId: userID
+  };
 
-    // MySQL won't allow undefined or empty strings.  This code
-    //changes any undefined value or empty string to null
-    Object.keys(newCharacter).forEach((stat) => {
-        if (newCharacter[stat] === undefined || newCharacter[stat] === "") {
-            newCharacter[stat] = 0;
-        }
+  // MySQL won't allow undefined or empty strings.  This code
+  //changes any undefined value or empty string to null
+  Object.keys(newCharacter).forEach((stat) => {
+    if (newCharacter[stat] === undefined || newCharacter[stat] === "") {
+      newCharacter[stat] = 0;
+    }
+  });
+  // Post new character then redirect to members page
+  console.log(newCharacter);
+  $.post("/api/characters", newCharacter)
+    .then(() => {
+      window.location.href = "/members";
     });
-    // Post new character then redirect to members page
-    console.log(newCharacter);
-    $.post("/api/characters", newCharacter)
-        .then(() => {
-            window.location.href = "/members";
-        });
 };
 
 // Delete button listener
